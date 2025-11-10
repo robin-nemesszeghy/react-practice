db3df164478954896975eb96d0421c816d17ac08
Robin <robinnem@outlook.com>
2025-11-10 13:53:51 -0500

    React Practice

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import Todo from "./components/Todo";
+import Title from "./components/Title";
+
+function App() {
+  return (
+    <div>
+      <Title />
+      <Todo />
+      <Todo />
+      <Todo />
+    </div>
+  );
+}
+
+export default App;
