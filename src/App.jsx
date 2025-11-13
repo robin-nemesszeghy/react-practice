d7fa4b8bf5c152ac9103b5418bf2a1a1b80e29eb
Robin <robinnem@outlook.com>
2025-11-13 15:19:31 -0500

    Routing Practice with React

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Home from "./pages/Home";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
 import Nav from "./components/Nav";
+import Users from "./pages/Users";
 
 function App() {
   return (
@@ -11,8 +12,7 @@ function App() {
       <Nav />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
+        <Route path="/users/:username" element={<Users />} />
       </Routes>
     </Router>
   );
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -4,8 +4,9 @@ function Nav() {
   return (
     <nav>
       <Link to="/">Home</Link>
-      <Link to="/about">About</Link>
-      <Link to="/contact">Contact</Link>
+      <Link to="/users/ronaldo">Ronaldo</Link>
+      <Link to="/users/messi">Messi</Link>
+      <Link to="/users/br4gg">David Bragg</Link>
     </nav>
   );
 }
