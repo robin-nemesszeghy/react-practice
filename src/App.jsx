2f9cb61c597c1e87e0b1066720960f7bfb8555c1
Robin <robinnem@outlook.com>
2025-11-13 14:13:52 -0500

    Routing with React

--- src/App.jsx
+++ src/App.jsx
@@ -1,59 +1,20 @@
 import "./App.css";
-import Todo from "./components/Todo";
-import Title from "./components/Title";
-import Modal from "./components/Modal";
-import React, { useState, useEffect } from "react";
-// import Counter from "./components/Counter";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Nav from "./components/Nav";
 
 function App() {
-  const [isModalOpen, setShowModal] = useState(false);
-
-  function onToDoDelete() {
-    setShowModal(true);
-  }
-
-  function confirmModal() {
-    setShowModal(false);
-  }
-
-  function cancelModal() {
-    setShowModal(false);
-  }
-
-  useEffect(() => {}, []);
-
   return (
-    <>
-      <Title />
-      <div>
-        <input type="text" onChange={(event) => {}} />
-        <button onClick={() => setShowModal(true)}>Add Todo</button>
-      </div>
-      <div className="todo__wrapper">
-        <Todo
-          onToDoDelete={onToDoDelete}
-          title="Finish Frontend Simplified"
-          paragraph="Code along with Frontend Simplified step-by-step."
-        />
-        <Todo
-          onToDoDelete={onToDoDelete}
-          title="Finish Interview Section"
-          paragraph="Finish every interview question in the next 6 weeks."
-        />
-        <Todo
-          onToDoDelete={onToDoDelete}
-          title="Land a $100K Job"
-          paragraph="Apply to 100 jobs."
-        />
-      </div>
-      {isModalOpen && (
-        <Modal
-          confirmModal={confirmModal}
-          cancelModal={cancelModal}
-          title="Are you sure you want to delete this task?"
-        />
-      )}
-    </>
+    <Router>
+      <Nav />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </Router>
   );
 }
 
